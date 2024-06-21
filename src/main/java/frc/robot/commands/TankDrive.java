746758181da30adc7a5ad5d37203796603d6a79c
Cody Nihsen <cnih150@gmail.com>
2024-06-20 20:42:33 -0400

    wrote tank drive command

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -1,1 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Xbox;
+
+public class TankDrive extends Command {
+  
+  private Drivetrain drivetrain;
+  private Xbox controller;
+
+  
+
+  /** Creates a new TankDrive. */
+  public TankDrive(Drivetrain d, Xbox c) {
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    drivetrain = d;
+    controller = c;
+
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drivetrain.setPower(controller.getLeftStickY(), controller.getRightStickY());
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
