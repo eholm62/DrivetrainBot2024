f0ef38702e53c9d375418c9d153bf37a9ebc6b85
braden <bradenmosier@gmail.com>
2024-09-05 20:02:51 -0400

    this used for freshmen training

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -13,37 +13,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
-  public Driveside left;
-  public Driveside right;
-
+  
   /** Creates a new Drivetrain. */
   public Drivetrain() {
-    left = new Driveside(Constants.FRONT_LEFT, Constants.BACK_LEFT);
-    right = new Driveside(Constants.FRONT_RIGHT, Constants.BACK_RIGHT);
+   
   }
 
-  public class Driveside {
-    public TalonSRX lead;
-    public TalonSRX follower;
-
-    public Driveside(int lead, int follower) {
-      this.lead = new TalonSRX(lead);
-      this.follower = new TalonSRX(follower);
-      this.follower.follow(this.lead);
-      this.lead.setNeutralMode(NeutralMode.Brake);
-      this.follower.setNeutralMode(NeutralMode.Brake);
-    }
-
-    public void setPower(double front, double back) {
-      this.lead.set(ControlMode.PercentOutput, front);
-      this.follower.set(ControlMode.PercentOutput, back);
-    }
-  }
-
-  public void setPower(double left, double right) {
-    this.left.setPower(left, left);
-    this.right.setPower(-right, -right);
-  }
+  
 
   @Override
   public void periodic() {
